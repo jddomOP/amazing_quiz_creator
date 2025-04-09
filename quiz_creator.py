0ad60192efe096073bd7d47bf35b5399de3d48cc
Jaydie <johndenieldomalaon@gmail.com>
2025-04-09 22:17:05 +0800

    Making psuedocode for the project

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+#This is made not purely by my own, I searched for much creativity of the project
+#PSEUDOCODE
+#START
+# 1. Display the Main menu
+#   - pick option between:
+#       "1. Create a quiz"
+#       "2. Exit"
+# 2. If user pick option 1 then the user is asked what the difficulty
+#

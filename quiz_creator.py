e42878b132702289bc7b8ab52970edfb63459fd6
Jaydie <johndenieldomalaon@gmail.com>
2025-04-09 22:58:38 +0800

    Done making the psuedocode. I added the pyfiglet library for the design of the title. Import pyfiglet and had a test run of the code to see which font I like most

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,9 +1,36 @@
 #This is made not purely by my own, I searched for much creativity of the project
 #PSEUDOCODE
 #START
-# 1. Display the Main menu
+# 1. Display the title using pyfiglet library
+# 2. Display the Main menu
 #   - pick option between:
 #       "1. Create a quiz"
 #       "2. Exit"
-# 2. If user pick option 1 then the user is asked what the difficulty
-#
+# 3. If the user selects "Create a quiz"
+#   - Prompt the user to enter what difficulty (Elementary(as easy), High School(as medium), General(as hard))
+#   - After selection, it will be in a loop for the user to input questions, choices and answers
+# 4. In question loop:
+#   - Ask the user to input questions
+#   - If the user enter "exit" then will be redirected to main menu
+#   - Otherwise, the user will be asked to input answers in the choices between a, b, c, d
+#   - Prompt the user to enter the correct answer (must be in a, b, c, d)
+#   - If the correct answer input is invalid, re-prompt until valid answer is given
+#   - Stores the question, choices, correct answer, and difficulties in a dictionary
+# 5. Write the question data to a text file in a structured format:
+#   - Write the question text
+#   - Write the difficulty
+#   - Write the options/choices each a, b, c, d
+#   - Write the correct answer
+#   - Write a separator line to divide questions in text file
+# 6. After saving the question, notify the user and repeat the loop to add more questions
+# 7. If the user selects 'Exit' in the main menu
+#   - Display goobye message
+#END
+
+import pyfiglet
+
+def the_title():
+    title = pyfiglet.figlet_format("Welcome to the Quiz Maker", font = 'graffiti')
+    print (title)
+
+the_title()

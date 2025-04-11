d720f909a3e574c2d052057c9fa333201b0ce08f
Jaydie <johndenieldomalaon@gmail.com>
2025-04-11 16:40:15 +0800

    add space

--- quiz_creator.py
+++ quiz_creator.py
@@ -76,7 +76,7 @@ def main_menu():
 def write_a_file(data, filename = 'quiz_questions_and_answers'):
     with open(filename, 'a',encoding = 'utf-8') as file:
         file.write("Q: " + data['question'] + "\n")
-        file.write("Difficulty: " +data['difficulty'] + "\n")
+        file.write("Difficulty: " + data['difficulty'] + "\n")
         file.write("a). " + data['a'] + "\n")
         file.write("b). " + data['b'] + "\n")
         file.write("c). " + data['c'] + "\n")
